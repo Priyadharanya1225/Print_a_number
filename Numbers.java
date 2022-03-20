e3c281f2c2886b0bbb1b24ee10719b6784cb27b8
Priyadharanya1225 <dharanyasathann@gmail.com>
2022-03-20 16:20:01 +0530

    the file is added

---
+++ Numbers.java
@@ -0,0 +1,14 @@
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		for(int j=1;j<=10;j++)
+		{
+			System.out.println(j);
+		}
+			
+		// TODO Auto-generated method stub
+
+	}
+
+}
